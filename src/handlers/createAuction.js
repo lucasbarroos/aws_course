97d3700932db0b0b420a9251c1ff2dabb794325e
Lucas Barros <lbarros.comp@gmail.com>
2020-10-16 04:40:25 -0300

    Inserting the validators to the create auctions

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,7 +1,9 @@
 import { v4 as uuid }  from 'uuid';
 import AWS from 'aws-sdk';
-import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
+import validator from '@middy/validator';
+import commonMiddleware from '../lib/commonMiddleware';
+import createAuctionsSchema from '../lib/schemas/createAuctionsSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -37,7 +39,8 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction)
+.use(validator({ inputSchema: createAuctionsSchema }));
 
 
 
